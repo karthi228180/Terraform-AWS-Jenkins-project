b17e29246463969920c66aaafbbd3c06910dd300
karthi228180 <karthikcl310@gmail.com>
2026-09-06 20:11:28 +0530

    App.test.js update

--- application-code/web-tier/src/App.test.js
+++ application-code/web-tier/src/App.test.js
@@ -1,8 +1,2 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// 
+import { render, screen } from '@testing-library/react'; import App from './App'; test('renders AWS 3-TIER WEB APP DEMO', () => { render(<App />); const headingElement = screen.getByText(/AWS 3-TIER WEB APP DEMO/i); expect(headingElement).toBeInTheDocument(); });
